13e2d1e45a19e0abf89b036dee1e8c5eaf910740
Tariq <tariq86hamid@gmail.com>
2022-05-31 11:06:32 -0700

    handled adding at the start and empty list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,13 +12,27 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp, *new, *h;
 
-	h = *head;
+	if (*head == NULL)
+	{
+		*head = malloc(sizeof(listint_t));
+		if (head == NULL)
+			return (NULL);
+		(*head)->n = number;
+		(*head)->next = NULL;
+		return (*head);
+	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 	new->next = NULL;
-
+	if ((*head)->n >= number)
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	h = *head;
 	while (h != NULL && (h->next) != NULL)
 	{
 		if (h->n <= number && (h->next)->n >= number)
