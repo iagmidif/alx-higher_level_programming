3fd8cd7d45ca549aa7d982d2fb1cdbff8144a4b7
Tariq <tariq86hamnid@gmail.com>
2022-06-22 12:08:59 -0700

    task 6 update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,24 +29,11 @@ class Square:
     def size(self):
         """
         Getter function for the private attribute size
-
-        Args:
-
-        Returns:
-            the value of the private attribute size
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter function for the private attribute size
-
-        Args:
-            value (int): value to be set for size
-
-        Returns:
-        """
         if type(value) is int:
             if value >= 0:
                 self.__size = value
@@ -59,25 +46,14 @@ class Square:
     def position(slef):
         """
         Getter function for the private attribute position
-
-        Args:
-
-        Returns:
-            the value of the priate attribute position
         """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """
-        Setter function for the private attribute position
-
-        Args:
-            value (tuple): value to be set for position
-
-        Returns:
-        """
-        if type(value) is tuple and len(value) is 2:
+        if type(value) is tuple and len(value) is 2 \
+                and type(value[0]) is int and type(value[1]) \
+                and value[0] >= 0 and value[0] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -85,18 +61,12 @@ class Square:
     def my_print(self):
         """
         A function that prints the square in stdout with the character #
-
-        Args:
-
-        Returns:
         """
-        if self.__size == 0:
-            print()
-        for y in range(self.__position[1]):
-            print()
-        for row in range(self.__size):
-            for x in range(self.__position[0]):
-                print(' ', end='')
-            for i in range(self.__size):
-                print("#", end='')
+        if self.__size > 0:
+            for y in range(self.__position[1]):
+                print()
+            for x in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print("#" * self.__size)
+        else:
             print()
