c68b3d0a4ca2adfeee3002cda04689f2a1a83d31
Tariq <tariq86hamnid@gmail.com>
2022-06-22 10:12:02 -0700

    task 0 update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines and empty class"""
+
+
 class Square:
-    """ An empty class that defines a square """
+    """An empty class that defines a square """
 
     def __init__(self):
+        """Instantiation function"""
         pass
