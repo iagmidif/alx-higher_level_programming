8fdad562383eec2ad71648e22f04a3ed8a6139e7
Tariq <tariq86hamnid@gmail.com>
2022-06-22 10:52:41 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that defines a Square class
+"""
+
+
+class Square:
+    """
+    A class that defines a Square
+    """
+
+    def __init__(self, size):
+        """Instantiation function"""
+
+        self.__size = size
