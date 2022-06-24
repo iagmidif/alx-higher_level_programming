467d8fa36801163261fb4070f56079283889ccee
Tariq <tariq86hamnid@gmail.com>
2022-06-24 10:30:28 -0700

    task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+Module that defines a Square class
+"""
+
+
+class Square:
+    """
+    A class that defines a Square
+    """
+
+    def __init__(self, size=0):
+        """Instantiation function"""
+        self.__size = size
+
+    def area(self):
+        """
+        A public instance method
+
+        Args:
+
+        Returns:
+            the area of the square
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """
+        Getter function for the private attribute size
+
+        Args:
+
+        Returns:
+            the value of the private attribute size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter function for the private attribute size
+
+        Args:
+            value (int): value to be set for size
+
+        Returns:
+        """
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def __eq__(self, other):
+        """Handles the behavior of == comparator"""
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        """Handles the behavior of != comparator"""
+        return (self.area() != other.area())
+
+    def __ge__(self, other):
+        """Handles the behavior of >= comparator"""
+        return (self.area() >= other.area())
+
+    def __le__(self, other):
+        """Handles the behavior of <= comparator"""
+        return (self.area() <= other.area())
+
+    def __gt__(self, other):
+        """Handles the behavior of > comparator"""
+        return (self.area() > other.area())
+
+    def __lt__(self, other):
+        """Handles the behavior of < comparator"""
+        return (self.area() < other.area())
