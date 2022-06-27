d1c2a1df067a4c1aca4f16be6930a3930ad9afd1
Tariq <tariq86hamnid@gmail.com>
2022-06-27 11:04:52 -0700

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Module that defines the class Rectangle
+"""
+
+
+class Rectangle:
+    """A class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation method
+
+        Args:
+            width (int, default=0): represents the width of the rectangle
+            height (int, default=0): represents the height of the rectangle
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ Getter method for the private attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for the private attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Method that calculates the area of the rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """ Method that calculates the perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return ((2 * self.__height) + (2 * self.__width))
+
+    def __str__(self):
+        """ Defines the behavior of str and print"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        data_str = ""
+        for row in range(self.__height):
+            for col in range(self.__width):
+                data_str += '#'
+            if row < self.__height - 1:
+                data_str += '\n'
+        return data_str
