0e1f70f037946c316fb6765b635cbd33cffebc7d
Tariq <tariq86hamnid@gmail.com>
2022-06-28 21:24:39 -0700

    task 2 update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
         last_name (string, default=""): last name to be used
     """
     if type(first_name) is str:
-        if type(last_name) is  str:
+        if type(last_name) is str:
             print("My name is {} {}".format(first_name, last_name))
         else:
             raise TypeError("last_name must be a string")
