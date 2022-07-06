5207bebcc443ff084b23c8d729249d602d1544e1
Tariq <tariq86hamnid@gmail.com>
2022-07-06 09:46:16 -0700

    task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that contains a class that inherits from int
+"""
+
+
+class MyInt(int):
+    """A class that inherits form int"""
+
+    def __eq__(self, other):
+        """Defines the == behaviour"""
+        return int(self) != other
+
+    def __ne__(self):
+        """Defines the != behaviour"""
+        return int(self) == other
