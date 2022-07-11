e071ae0916535c509a8bdf6aa256667ed3ec0472
Tariq <tariq86hamnid@gmail.com>
2022-07-11 12:24:53 -0700

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,13 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of a `list_objs` to a file"""
+        lst = []
+        if list_objs is not None or len(list_objs) > 0:
+            for obj in list_objs:
+                lst.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", "w") as f:
+            f.write(Base.to_json_string(lst))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -72,5 +72,66 @@ class TestToJSONString(TestCase):
             Base.to_json_string("hello", [1, 2])
 
 
+class TestSaveToFile(TestCase):
+
+    def tearDown(self):
+        Base._Base__nb_objects = 0
+        try:
+            os.remove("Rectangle.json")
+        except Exception:
+            pass
+        try:
+            os.remove("Square.json")
+        except Exception:
+            pass
+
+    def test_save_to_file_rectangle(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        Rectangle.save_to_file([r1])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(len(f.read()), 53)
+
+    def test_save_to_file_square(self):
+        s1 = Square(2, 3, 4, 5)
+        Square.save_to_file([s1])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 38)
+
+    def test_save_to_file_multi_rectangles(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(5, 2, 9, 11)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(len(f.read()), 106)
+
+    def test_save_to_file_square(self):
+        s1 = Square(2, 3, 4, 5)
+        s2 = Square(9, 8, 7, 6)
+        Square.save_to_file([s1, s2])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 76)
+
+    def test_save_to_file_overwrite(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        Square.save_to_file([r1])
+        r2 = Rectangle(1, 7, 2, 8)
+        Square.save_to_file([r2])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 52)
+
+    def test_save_to_file_empty_lst(self):
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_save_to_file_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle.save_to_file()
+
+    def test_save_to_file_too_many_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle.save_to_file("hello", [1, 2])
+
+
 if __name__ == '__main__':
     unittest.main()
